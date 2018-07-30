80d41317c529be196fd3bb6b5d27eea959db75cb
Tom Gow-Gates <tomrgg@gmail.com>
2018-07-30 15:07:06 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+
+
+class QuestionsController < ApplicationController
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    ask
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.last == "?"
+      @answer = "Silly question, get dressed and go to work!."
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
